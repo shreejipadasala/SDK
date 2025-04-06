6b14933bdae65738301df159fdc121f0a4635c7f
shreejipadasala <shreejipadasala21@gmail.com>
2025-04-06 18:04:25 +0530

    Update urls.py

--- sdkvism/sdkvism/urls.py
+++ sdkvism/sdkvism/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
