957a969d029988d9c1b8a7ccbd9f378bc0e83426
shreejipadasala <shreejipadasala21@gmail.com>
2025-04-06 17:48:23 +0530

    Update urls.py

--- sdkvism/sdkvism/urls.py
+++ sdkvism/sdkvism/urls.py
@@ -19,5 +19,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('sdkreact/',include('sdkreact.urls'))
+    path('sdkreact/',include('sdkreact.urls')),
+    path('', lambda request: HttpResponse("Welcome to the homepage!"))
 ]
