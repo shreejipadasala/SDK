f14f7b8ed524f3e470fd0c0937a04cf016980af2
shreejipadasala <shreejipadasala21@gmail.com>
2025-04-06 19:16:26 +0530

    changes

--- sdkvism/sdkvism/settings.py
+++ sdkvism/sdkvism/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'sdkvism', 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'sdkfrontend', 'build', 'static')]
 
 
 # Default primary key field type
