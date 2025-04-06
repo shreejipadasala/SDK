2b4b5cfbfd3dae998c4cb695f4d48285d9d360ea
shreejipadasala <shreejipadasala21@gmail.com>
2025-04-06 17:41:46 +0530

    Host Allowed of Render.py

--- sdkvism/sdkvism/settings.py
+++ sdkvism/sdkvism/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-rbhuegc6s7muma62qzx+%jl1pu1!qd(nex*h$x-z)pc)-&&gsc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sdk-1.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
